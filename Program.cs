8250fdb3ab22b63ad2d325c4bf8c83635c247a65
Thales Closs <thalescloss@vipcommerce.com.br>
2022-10-18 20:05:04 -0400

    Solução do desafio

--- Program.cs
+++ Program.cs
@@ -1,25 +1,70 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+Reserva BaseTeste(int capacidade, decimal valorDiaria, int diasReservados)
+{
+    Suite suite = new Suite(tipoSuite: "Premium", capacidade, valorDiaria);
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+    Reserva reserva = new Reserva(diasReservados);
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    reserva.CadastrarSuite(suite);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+    return reserva;
+}
+List<Pessoa> CriarHospedes(int quantidadeHospedes, string prefixo = "")
+{
+    return new List<Pessoa>(Enumerable.Range(1, quantidadeHospedes).Select((value) => new Pessoa($"Hóspede {prefixo} {value}")));
+}
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+void ExecutarTestePadrao(int capacidade, decimal valorDiaria, int diasReservados, int quantidadeHospedes)
+{
+    try
+    {
+        var reserva = BaseTeste(capacidade, valorDiaria, diasReservados);
+        var hospedes = CriarHospedes(quantidadeHospedes);
+        reserva.CadastrarHospedes(hospedes);
+        Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+        Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria():C}");
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine($"Erro ao adicionar hóspedes: {error.Message}");
+    }
+}
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+void ExecutarTesteAdicionandoMaisHospedes(int capacidade, decimal valorDiaria, int diasReservados, List<int> quantidadeHospedesPorVez)
+{
+    try
+    {
+        var reserva = BaseTeste(capacidade, valorDiaria, diasReservados);
+        foreach (var (quantidadeHospedes, prefixo) in quantidadeHospedesPorVez.Select((value, index) => (value, $"add-{index}")))
+        {
+            reserva.CadastrarHospedes(CriarHospedes(quantidadeHospedes, prefixo));
+        }
+
+        Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+        Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria():C}");
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine($"Erro ao adicionar hóspedes: {error.Message}");
+    }
+}
+Console.WriteLine("Teste padrão:\n");
+ExecutarTestePadrao(capacidade: 2, valorDiaria: 30, diasReservados: 5, quantidadeHospedes: 2);
+ExecutarTestePadrao(capacidade: 2, valorDiaria: 30, diasReservados: 10, quantidadeHospedes: 2);
+ExecutarTestePadrao(capacidade: 2, valorDiaria: 30, diasReservados: 20, quantidadeHospedes: 1);
+ExecutarTestePadrao(capacidade: 1, valorDiaria: 30, diasReservados: 10, quantidadeHospedes: 2);
+ExecutarTestePadrao(capacidade: 10, valorDiaria: 3000, diasReservados: 10, quantidadeHospedes: 20);
+Console.WriteLine('\n');
+
+Console.WriteLine("Teste adicionando mais hóspedes em uma reserva existente:\n");
+ExecutarTesteAdicionandoMaisHospedes(capacidade: 4, valorDiaria: 30, diasReservados: 5, new List<int> { 2, 2 });
+ExecutarTesteAdicionandoMaisHospedes(capacidade: 2, valorDiaria: 30, diasReservados: 5, new List<int> { 2, 2 });
+ExecutarTesteAdicionandoMaisHospedes(capacidade: 2, valorDiaria: 30, diasReservados: 5, new List<int> { 1, 2 });
+ExecutarTesteAdicionandoMaisHospedes(capacidade: 2, valorDiaria: 30, diasReservados: 5, new List<int> { 1, 1, 3 });
